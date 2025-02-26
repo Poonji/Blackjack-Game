7b72cee882e3a9d26c6bd663277df5f29c62a850
Poonji <poonjij21@gmail.com>
2025-02-27 03:08:55 +0530

    added new card and generate random numbr of card

--- index.js
+++ index.js
@@ -1,27 +1,67 @@
-let firstCard=10
-let secondCard=8
+function getRandomCard()
+{
+    let randomNumber=Math.floor(Math.random()*13)+1
+    if(randomNumber>10)
+    {
+        return 10
+    }
+    else if(randomNumber==1){
+        return 1
+    }
+    else{
+        return randomNumber
+    }
+}
+let firstCard=getRandomCard()
+let secondCard=getRandomCard()
+let cards=[firstCard, secondCard]
 let sum=firstCard+secondCard
-let hasBlackJack=false
-let isAlive=true
 
 let message = " "
+let messageEl=document.getElementById("message-el")
+//let sumEl=document.getElementById("sum-el")
+//0R
+let sumEl=document.querySelector("#sum-el")
+// its a borader term css selector, after selecting querry we will define id or class
+let cardsE1=document.getElementById("cards-el")
+
 function startGame()
 {
+    renderGame()
+}
+
+function renderGame()
+{
+    cardsE1.textContent="Cards: "
+    for(let i=0;i<cards.length;i++)
+    {
+        cardsE1.textContent+=cards[i] + "  "
+    }
+    sumEl.textContent="Sum: " + sum
 
 if(sum<=20)
 {
-    message="Do you want to draw one more card?😊"
+    message="Do you want to draw one more card?"
 }
 
 else if (sum===21)
 {
-    message="OHOOOOO you got blackjack 😍"
+    message="OHOOOOO you got blackjack"
     hasBlackJack=true
 }
 else{
-    message="you have got out of the game😭"
+    message="you have got out of the game"
     isAlive=false
 }
-console.log(message)
+messageEl.innerText=message // you can also use .textcontent
 //console.log(hasBlackJack)
+}
+
+function newCard()
+{
+    //console.log("Drawing a new card from the deck!")
+    let card=getRandomCard()
+    sum+=card
+    cards.push(card)
+    startGame()
 }
