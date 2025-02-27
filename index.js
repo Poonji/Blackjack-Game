b3c7d540f6746d07488548f8940a1d43be6df2f3
Poonji <poonjij21@gmail.com>
2025-02-27 16:13:30 +0530

    added name and credit by objects

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+let player={
+    name:"Poonji",
+    chips: 200
+}
+let cards=[]
+let sum=0
+let hasBlackJack=false
+let isAlive=false
+let message = " "
+let messageEl= document.getElementById("message-el")
+//let sumEl=document.getElementById("sum-el")
+//0R
+let sumEl= document.querySelector("#sum-el")
+// its a borader term css selector, after selecting querry we will define id or class
+let cardsE1= document.getElementById("cards-el")
+//console.log(cards)
+let playerEl=document.getElementById("player-el")
+playerEl.textContent= player.name + ": $"+ player.chips
+
 function getRandomCard()
 {
     let randomNumber=Math.floor(Math.random()*13)+1
@@ -6,28 +25,22 @@ function getRandomCard()
         return 10
     }
     else if(randomNumber==1){
-        return 1
+        return 11
     }
     else{
         return randomNumber
     }
 }
-let firstCard=getRandomCard()
-let secondCard=getRandomCard()
-let cards=[firstCard, secondCard]
-let sum=firstCard+secondCard
-
-let message = " "
-let messageEl=document.getElementById("message-el")
-//let sumEl=document.getElementById("sum-el")
-//0R
-let sumEl=document.querySelector("#sum-el")
-// its a borader term css selector, after selecting querry we will define id or class
-let cardsE1=document.getElementById("cards-el")
 
 function startGame()
 {
+    isAlive=true
+    let firstCard=getRandomCard()
+    let secondCard=getRandomCard()
+    cards=[firstCard,secondCard]
+    sum=firstCard+secondCard
     renderGame()
+
 }
 
 function renderGame()
@@ -59,9 +72,12 @@ messageEl.innerText=message // you can also use .textcontent
 
 function newCard()
 {
+    if(isAlive==true && hasBlackJack==false)
+    {
     //console.log("Drawing a new card from the deck!")
     let card=getRandomCard()
     sum+=card
     cards.push(card)
-    startGame()
+    renderGame()
+}
 }
